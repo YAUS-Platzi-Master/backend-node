ae64e27695f2dae825029560939168d8d392b2ff
gposso <gilberto.posso@gmail.com>
2020-08-25 15:50:54 -0500

    feature main route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const helmet = require('helmet')
 const { handleFatalError } = require('./utils/handler')
+const { validateShortUrl, saveHits } = require('./lib/db')
 const { config } = require('./config')
 
 const app = express()
 
+// middleware
 app.use(express.json())
 app.use(helmet())
 
-process.on('uncaughtException', handleFatalError)
-process.on('unhandledRejection', handleFatalError)
+// main route
+app.get(/^\/[a-z]+$/i, async function (req, res) {
+  const shortUrl = await req.url.slice(1)
+  const longUrl = await validateShortUrl(shortUrl)
+
+  if (longUrl === undefined) {
+    res.redirect('/')
+  } else {
+    saveHits(longUrl.id, req)
+    res.redirect(longUrl.long_url)
+  }
+})
+
+// page not found
+app.get('/*', function (req, res) {
+  res.status(404).json('Page Not Found')
+})
 
 const server = app.listen(config.port, function () {
   console.log(`Listening on port ${server.address().port}`)
 })
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
